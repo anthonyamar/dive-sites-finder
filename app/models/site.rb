597c7940cb5006279cb7bb59b57cd62b790af0f8
anthonyamar <anthonyamar@labodessons.fr>
2023-11-05 17:59:31 +0100

    create datasets and csv importer

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,12 +1,12 @@
 class Site < ApplicationRecord
-  enum bow: { salt: 'salt', artificial: 'artificial', fresh: 'fresh' }
+  enum bow: { salt: 'salt', artificial: 'artificial', fresh: 'fresh', unknown: 'unknown' }
 
-  validates :name, presence: true, length: { maximum: 90 }
+  validates :name, presence: true, length: { maximum: 255 }
   validates :bow, presence: true, inclusion: { in: bows.keys }
   validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
   validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
-  validates :address, presence: true, length: { maximum: 255 }
-  validates :region, presence: true, length: { maximum: 255 }
-  validates :country, presence: true, length: { maximum: 255 }
+  validates :address, length: { maximum: 255 }, allow_blank: true
+  validates :region, length: { maximum: 255 }, allow_blank: true
+  validates :country, length: { maximum: 255 }, allow_blank: true
   validates :is_private, inclusion: { in: [true, false] }
 end
