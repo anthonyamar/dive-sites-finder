6f91b262e1de8da6d5dd714c5890b969b4164e63
anthonyamar <anthonyamar@labodessons.fr>
2024-01-23 23:47:27 +0100

    add kind water or continent to geo group

--- app/models/geo_group.rb
+++ app/models/geo_group.rb
@@ -1,6 +1,7 @@
 class GeoGroup < ApplicationRecord
   
   reverse_geocoded_by :latitude, :longitude
+  enum kind: { water: 'water', continent: 'continent' }
 
   # ============= relations ============
   
@@ -17,6 +18,7 @@ class GeoGroup < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
   validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
+  validates :kind, presence: true, inclusion: { in: kind.keys }
 
 
   # ============= scopes ============
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_23_201157) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_23_224159) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -144,6 +144,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_23_201157) do
     t.integer "cities", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "kind"
   end
 
   create_table "geo_groups_countries", force: :cascade do |t|
