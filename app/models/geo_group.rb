98f9322d90519ef036f73ce86aece13bb5aa9212
anthonyamar <anthonyamar@labodessons.fr>
2024-01-23 23:56:08 +0100

    update view to split geo_groups per continent and waters

--- app/models/geo_group.rb
+++ app/models/geo_group.rb
@@ -18,7 +18,7 @@ class GeoGroup < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :longitude, presence: true, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
   validates :latitude, presence: true, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
-  validates :kind, presence: true, inclusion: { in: kind.keys }
+  validates :kind, presence: true, inclusion: { in: kinds.keys }
 
 
   # ============= scopes ============
