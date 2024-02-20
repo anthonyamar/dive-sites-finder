37f884cefb159ffc4544a6a37cd255673efba910
anthonyamar <anthonyamar@labodessons.fr>
2024-02-20 22:06:15 +0100

    generate the model and migration data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_20_193300) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_20_210103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -167,7 +167,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_20_193300) do
     t.index ["activity_id"], name: "index_dive_centers_activities_on_activity_id"
     t.index ["dive_center_id"], name: "index_dive_centers_activities_on_dive_center_id"
   end
-  
+
   create_table "dive_centers_affiliations", force: :cascade do |t|
     t.bigint "dive_center_id", null: false
     t.bigint "affiliation_id", null: false
@@ -226,12 +226,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_20_193300) do
     t.index ["geo_group_id"], name: "index_geo_groups_countries_on_geo_group_id"
   end
 
-  create_table "prestations", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "regions", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
@@ -247,6 +241,27 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_20_193300) do
     t.string "slug"
     t.index ["country_id"], name: "index_regions_on_country_id"
     t.index ["slug"], name: "index_regions_on_slug", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
