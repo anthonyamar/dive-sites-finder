4ca640a87591d3f664ce420cd869d4957f7ca38b
anthonyamar <anthonyamar@labodessons.fr>
2024-02-24 07:27:47 +0100

    add role column to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :validatable, :trackable
+    :recoverable, :rememberable, :validatable, :trackable
+  
+  enum role: { user: 'user', admin: 'admin' }
+
+  # ============= relations ============
+  
+  
+  
+  # ============= validations ============
+  
+  validates :role, presence: true, inclusion: { in: roles.keys }
+  
+  
+  # ============= scopes ============
+  
+  
+  
+  # ============= methods ===========
+  
+  
+  
 end
